0a69eccd2d4b0722e36bdedf5235ce4900df6162
you21979 <you2197901@gmail.com>
2015-01-15 13:57:48 +0900

    mod

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ exports.conn = require('./lib/conn');
 exports.req = require('./lib/req');
 exports.tx = require('./lib/tx');
 exports.orderbook = require('./lib/orderbook');
+exports.ripple = require('ripple-lib');
+exports.Promise = require('bluebird');
+exports._ = require('lodash');
